831d6853d9ee1ab8fa0c44d35417ff19cecb2b88
notchcoder <kokiuaxdx@gmail.com>
2023-08-27 23:45:37 +0900

    refactor: アーキテクチャ整備

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:maiporarisu/ui/navigation/maiporarisu_navigation.dart';
+import 'package:maiporarisu/ui/styles/color.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaiporarisuApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MaiporarisuApp extends StatelessWidget {
+  const MaiporarisuApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Maiporarisu',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: MaiporarisuColor.keyColor),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const MaiporarisuNavigation(),
     );
   }
 }
