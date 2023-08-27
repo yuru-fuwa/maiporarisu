f5f27574eb3c1f610373387e2029fc5f2b17cc63
komugi8 <120884254+komugi8@users.noreply.github.com>
2023-08-28 02:09:20 +0900

    Update lib/ui/screens/setting_screen/setting_screen.dart
    Co-authored-by: chnotchy <79978827+chnotchy@users.noreply.github.com>

--- lib/ui/screens/setting_screen/setting_screen.dart
+++ lib/ui/screens/setting_screen/setting_screen.dart
@@ -5,6 +5,10 @@ class SettingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text("Setting"),
+      ),
+    );
   }
 }
