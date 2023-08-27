8f463e3260c221ae966175397cd99f64996b1634
komugi8 <8448eito@gmail.com>
2023-08-28 01:41:09 +0900

    feat: 画面作成と画面遷移の実装

--- lib/ui/navigation/maiporarisu_navigation.dart
+++ lib/ui/navigation/maiporarisu_navigation.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
 
-class MaiporarisuNavigation extends StatelessWidget {
+class MaiporarisuNavigation extends StatefulWidget {
   const MaiporarisuNavigation({super.key});
 
   @override
+  State<MaiporarisuNavigation> createState() => _MaiporarisuNavigationState();
+}
+
+class _MaiporarisuNavigationState extends State<MaiporarisuNavigation> {
+  var _currentPageIndex = 0;
+  
+  final _pages = <Widget>[
+    Container(
+      alignment: Alignment.center,
+      child: const Text('Home'),
+    ),
+    Container(
+      alignment: Alignment.center,
+      child: const Text('Schedule'),
+    ),
+    Container(
+      alignment: Alignment.center,
+      child: const Text('Setting'),
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ページ切り替え'),
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        selectedIndex: _currentPageIndex,
+        elevation: 10,
+        height: 100,
+	labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+        destinations: const <Widget>[
+          NavigationDestination(
+              icon: Icon(Icons.home_outlined),
+              label: 'ホーム',
+              selectedIcon: Icon(Icons.home_rounded),
+              ),
+          NavigationDestination(
+              icon: Icon(Icons.today_outlined),
+              label: 'スケジュール',
+              selectedIcon: Icon(Icons.today_rounded),
+              ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined),
+            label: '設定',
+            selectedIcon: Icon(Icons.settings_rounded),
+          ),
+        ],
       ),
+      body: _pages[_currentPageIndex],
     );
   }
 }
---
+++ lib/ui/screens/schedule_screen/schedule_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScheduleScreen extends StatelessWidget {
+  const ScheduleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
