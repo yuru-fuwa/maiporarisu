e100d12dbf55f0126db02904cfb61296b79c560b
komugi8 <120884254+komugi8@users.noreply.github.com>
2023-08-28 02:09:11 +0900

    Update lib/ui/screens/schedule_screen/schedule_screen.dart
    Co-authored-by: chnotchy <79978827+chnotchy@users.noreply.github.com>

--- lib/ui/screens/schedule_screen/schedule_screen.dart
+++ lib/ui/screens/schedule_screen/schedule_screen.dart
@@ -5,6 +5,10 @@ class ScheduleScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text("Schedule"),
+      ),
+    );
   }
 }
