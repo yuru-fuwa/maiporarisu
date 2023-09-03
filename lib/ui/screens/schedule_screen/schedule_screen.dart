d14471a6d1bf2a56f9868e50976f891650d73270
komugi8 <8448eito@gmail.com>
2023-09-03 19:43:36 +0900

    feat: 位置情報の取得と権限の許可

--- lib/ui/screens/schedule_screen/schedule_screen.dart
+++ lib/ui/screens/schedule_screen/schedule_screen.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:maiporarisu/data/location/location.dart';
 
-class ScheduleScreen extends StatelessWidget {
+class ScheduleScreen extends StatefulWidget {
   const ScheduleScreen({super.key});
 
   @override
+  State<ScheduleScreen> createState() => _ScheduleScreenState();
+}
+
+class _ScheduleScreenState extends State<ScheduleScreen> {
+  Location location = Location();
+
+  String _locationview = "no data";
+
+  Future<void> getLocation() async {
+    BuildContext context = this.context;
+    Position pos = await location.determinePosition(context);
+    setState(() {
+      _locationview =
+          "TimeStamp: ${pos.timestamp}, Latitude: ${pos.latitude}, Longitude: ${pos.longitude}";
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Schedule"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _locationview,
+            )
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: getLocation, child: const Icon(Icons.location_on)),
     );
   }
 }
