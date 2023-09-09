c18bc4a2f01b38d8f358d9fc0e5282165058b32c
komugi8 <8448eito@gmail.com>
2023-09-10 01:21:13 +0900

    feat: サーバーの移行

--- lib/data/app_constants/app_constants.dart
+++ lib/data/app_constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String BASE_URL = "http://192.168.0.108:8080";
+  static const String BASE_URL = "https://wa-maiporarisu.azurewebsites.net";
   static const String GET_TASKS = "/gettasks";
   static const String POST_TASK = "/create";
   static const String GET_TASK = "/gettask";
