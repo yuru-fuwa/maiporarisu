fcc9ec3fcf24a0ef435d245aab8a0efa372b630e
notchcoder <kokiuaxdx@gmail.com>
2023-09-10 22:23:05 +0900

    feat: `BASE_URL`の初期化処理

--- lib/data/app_constants/app_constants.dart
+++ lib/data/app_constants/app_constants.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
-  static String baseUrl = dotenv.env['VAR_NAME'] ?? "";
+  static String baseUrl = "";
   static const String getTasks = "/gettasks";
   static const String postTask = "/create";
   static const String getTask = "/gettask";
+
+  void init() {
+    baseUrl = dotenv.env['BASE_URL'] ?? "";
+  }
 }
