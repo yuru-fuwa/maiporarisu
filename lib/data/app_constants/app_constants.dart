504568dc54f53f01a4a321cddcf56b596e68905c
komugi8 <120884254+komugi8@users.noreply.github.com>
2023-10-06 16:53:19 +0900

    Update lib/data/app_constants/app_constants.dart
    Co-authored-by: chnotchy <79978827+chnotchy@users.noreply.github.com>

--- lib/data/app_constants/app_constants.dart
+++ lib/data/app_constants/app_constants.dart
@@ -3,10 +3,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class AppConstants {
   static String baseUrl = '';
   static const String getTasks = '/gettasks';
-  static const String postTask = '/create';
   static const String getTask = '/gettask';
+  static const String postTask = '/create';
 
-  get getTaskUrl => baseUrl + getTasks;
+  get getTasksUrl => baseUrl + getTasks;
+  get getTaskUrl => baseUrl + getTask;
   get postTaskUrl => baseUrl + postTask;
 
   void init() {
