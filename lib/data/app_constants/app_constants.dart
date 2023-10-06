dc2663f2a468abe66554fa5209242f5b38ba42c7
komugi8 <8448eito@gmail.com>
2023-10-06 16:01:57 +0900

    chore: data_controllerをfreezedのtaskモデルに対応, 可読性の向上

--- lib/data/app_constants/app_constants.dart
+++ lib/data/app_constants/app_constants.dart
@@ -6,6 +6,9 @@ class AppConstants {
   static const String postTask = '/create';
   static const String getTask = '/gettask';
 
+  get getTaskUrl => baseUrl + getTasks;
+  get postTaskUrl => baseUrl + postTask;
+
   void init() {
     baseUrl = dotenv.env['BASE_URL'] ?? '';
   }
