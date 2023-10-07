b7f36f7fba60b7ad4edb3d355b7018a778a0313d
notchcoder <kokiuaxdx@gmail.com>
2023-10-07 20:52:03 +0900

    refactor: プライベート変数名の変更

--- lib/data/controllers/user_request/user_request.dart
+++ lib/data/controllers/user_request/user_request.dart
@@ -5,12 +5,12 @@ import 'package:maiporarisu/data/app_constants/app_constants.dart';
 import 'package:maiporarisu/data/model/task_model/task_model.dart';
 
 class UserRequest {
-  Uri getTasksUrl = Uri.parse(AppConstants.getTasksUrl);
-  Uri postTaskUrl = Uri.parse(AppConstants.postTaskUrl);
+  final Uri _getTasksUrl = Uri.parse(AppConstants.getTasksUrl);
+  final Uri _postTaskUrl = Uri.parse(AppConstants.postTaskUrl);
 
   Future<List<Task>> getAllTasks() async {
     http.Response response = await http.get(
-      getTasksUrl,
+      _getTasksUrl,
       headers: {
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -31,7 +31,7 @@ class UserRequest {
 
   Future<void> postTask(String time, String name) async {
     http.Response response = await http.post(
-      postTaskUrl,
+      _postTaskUrl,
       headers: {
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -42,7 +42,7 @@ class UserRequest {
         },
       ),
     );
-    debugPrint(postTaskUrl.toString());
+    debugPrint(_postTaskUrl.toString());
     if (response.statusCode == 200) {
       debugPrint('data post successful');
     } else {
