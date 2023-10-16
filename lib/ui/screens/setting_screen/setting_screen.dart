412f03930ae9ae18152427b7d63b86f095f13c89
kototo522 <huyejiben@gmail.com>
2023-10-16 22:51:32 +0900

    add: 仮のitemをリスト表示させました

--- lib/ui/screens/setting_screen/setting_screen.dart
+++ lib/ui/screens/setting_screen/setting_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
 class SettingScreen extends StatelessWidget {
-  const SettingScreen({super.key});
+  const SettingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Setting'),
+    final List<Map<String, String>> items = [
+      {
+        'title': '設定1',
+        'text': '設定1の説明',
+      },
+      {
+        'title': '設定2',
+        'text': '設定2の説明',
+      },
+      {
+        'title': '設定2',
+        'text': '設定2の説明',
+      },
+    ];
+
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]['title']!),
+            subtitle: Text(items[index]['text']!),
+          );
+        },
       ),
     );
   }
