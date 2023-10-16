dd61650347d0ab4ffcfd552c26900968390f5829
kototo522 <huyejiben@gmail.com>
2023-10-16 23:05:36 +0900

    add: 大まかなスタイルの追加

--- lib/ui/screens/setting_screen/setting_screen.dart
+++ lib/ui/screens/setting_screen/setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maiporarisu/ui/styles/color.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -24,11 +25,47 @@ class SettingScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: items.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(items[index]['title']!),
-            subtitle: Text(items[index]['text']!),
+          return SettingListItem(
+            title: items[index]['title']!,
+            subtitle: items[index]['text']!,
           );
         },
+      ),
+    );
+  }
+}
+
+class SettingListItem extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  SettingListItem({required this.title, required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.green[50], // 可愛い色に変更
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.all(8),
+      child: ListTile(
+        leading: const Icon(Icons.add_alert, color: MaiporarisuColor.keyColor),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: MaiporarisuColor.keyColor,
+          ),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: const TextStyle(
+            fontSize: 16,
+            color: MaiporarisuColor.keyColor,
+          ),
+        ),
       ),
     );
   }
