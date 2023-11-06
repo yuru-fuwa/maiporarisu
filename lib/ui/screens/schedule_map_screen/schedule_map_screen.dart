3d50136da04e95034ef0ff3251348f565dd2e2bf
chnotchy <kokiuaxdx@gmail.com>
2023-11-07 02:05:44 +0900

    feat: レイアウト変更

--- lib/ui/screens/schedule_map_screen/schedule_map_screen.dart
+++ lib/ui/screens/schedule_map_screen/schedule_map_screen.dart
@@ -32,61 +32,54 @@ class _ScheduleMapScreenState extends State<ScheduleMapScreen> {
   Widget build(BuildContext context) {
     final userRequest = UserRequest(isMock: true);
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            FutureBuilder<List<Task>>(
-              future: userRequest.getAllTasks(),
-              builder:
-                  (BuildContext context, AsyncSnapshot<List<Task>> snapshot) {
-                final taskList = snapshot.data ?? <Task>[];
-                return ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: taskList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final task = taskList[index];
-                    return TaskItem(task: task);
-                  },
-                );
-              },
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Color.alphaBlend(
+          Theme.of(context).colorScheme.primary.withOpacity(0.08),
+          Theme.of(context).colorScheme.background,
+        ).withOpacity(0.75),
+        surfaceTintColor: Colors.transparent,
+        centerTitle: true,
+        title: const Text('まいぽらりす'),
+      ),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          const GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(35.681236, 139.767125), // デフォルト: 東京駅
+              zoom: 16.0,
             ),
-            const SizedBox(height: 16),
-            Container(
-              alignment: Alignment.center,
-              height: 200,
-              padding: const EdgeInsets.all(16),
-              margin: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: _locationview == 'no data'
-                  ? Icon(
-                      Icons.location_off,
-                      size: 80,
-                      color: Theme.of(context)
-                          .colorScheme
-                          .onBackground
-                          .withOpacity(0.25),
-                    )
-                  : Text(_locationview),
-            ),
-            const SizedBox(
-              height: 300,
-              child: Padding(
-                padding: EdgeInsets.all(30),
-                child: GoogleMap(
-                  initialCameraPosition: CameraPosition(
-                    target: LatLng(35.681236, 139.767125), // デフォルト: 東京駅
-                    zoom: 16.0,
-                  ),
-                  // その他のGoogleMapの設定
+            // その他のGoogleMapの設定
+          ),
+          FutureBuilder<List<Task>>(
+            future: userRequest.getAllTasks(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Task>> snapshot) {
+              final taskList = snapshot.data ?? <Task>[];
+              return ListView.builder(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).padding.top + kToolbarHeight,
+                  bottom: MediaQuery.of(context).padding.bottom,
                 ),
-              ),
+                itemCount: taskList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final task = taskList[index];
+                  return TaskItem(task: task);
+                },
+              );
+            },
+          ),
+          Container(
+            height: MediaQuery.of(context).padding.bottom,
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height -
+                  MediaQuery.of(context).padding.bottom,
             ),
-          ],
-        ),
+            color: Colors.transparent,
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: getLocation,
