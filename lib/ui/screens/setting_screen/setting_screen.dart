329730731c02b84af14fa1ba796d01f36a7a64a6
cardseditor <toshiki.u314@gmail.com>
2023-11-11 12:51:46 +0900

    feat: Add QR scanner screen and button

--- lib/ui/screens/setting_screen/setting_screen.dart
+++ lib/ui/screens/setting_screen/setting_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:maiporarisu/ui/screens/qr_screen/qr_screen.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Setting'),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            const Text('Setting Screen'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const QRViewExample(),
+                  ),
+                );
+              },
+              child: const Text('qrView'),
+            ),
+          ],
+        ),
       ),
     );
   }
