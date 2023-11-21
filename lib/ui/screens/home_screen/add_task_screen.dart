b3b8e684965093e1920e05c46f49e7ce657f3386
tami0304 <hiyokotaro.1101@gmail.com>
2023-11-21 19:14:18 +0900

    add: タスク入力画面の追加

---
+++ lib/ui/screens/home_screen/add_task_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("task追加画面だよ")),
+    );
+  }
+}
