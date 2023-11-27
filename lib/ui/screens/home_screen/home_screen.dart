9419bd8d3d3b6ff7a6b6cb49b615cce59bbb63e2
komugi8 <8448eito@gmail.com>
2023-11-27 23:18:13 +0900

    post時のformat修正

--- lib/ui/screens/home_screen/home_screen.dart
+++ lib/ui/screens/home_screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:intl/intl.dart';
 import 'package:maiporarisu/data/controllers/user_request/user_request.dart';
 import 'package:maiporarisu/ui/screens/home_screen/hook/use_home_screen_state.dart';
 
@@ -79,7 +80,15 @@ class HomeScreen extends HookWidget {
               onPressed: () {
                 if (taskValidation()) {
                   userRequest.postTask(
-                    '${state.dateTime.year}.${state.dateTime.month}.${state.dateTime.day} ${state.timeOfDay.hour}:${state.timeOfDay.minute}',
+                    DateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(
+                      DateTime(
+                        state.dateTime.year,
+                        state.dateTime.month,
+                        state.dateTime.day,
+                        state.timeOfDay.hour,
+                        state.timeOfDay.minute,
+                      ),
+                    ),
                     taskController.text.trim(),
                   );
                 }
