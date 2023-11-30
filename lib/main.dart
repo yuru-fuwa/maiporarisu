9befa8074f0e64468f8999ebb655a2b6f8dc47d3
komugi8 <8448eito@gmail.com>
2023-11-30 15:31:22 +0900

    ファイル構成の修正

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:maiporarisu/data/service/user_request.dart';
+import 'package:maiporarisu/data/service/service_constants.dart';
 import 'package:maiporarisu/ui/screens/schedule_map_screen/map_schedule_screen.dart';
 import 'package:maiporarisu/ui/styles/color.dart';
 
