3c34f61b4efc2c4646c1b1a3ff31e92cd4292643
komugi8 <8448eito@gmail.com>
2023-11-30 17:15:49 +0900

    タスク時刻差のgetterの拡張

--- lib/ui/screens/home_screen/hook/use_home_screen_state.dart
+++ lib/ui/screens/home_screen/hook/use_home_screen_state.dart
@@ -39,18 +39,31 @@ class HomeScreenState with _$HomeScreenState {
     var difference = dateTime
         .add(Duration(hours: timeOfDay.hour, minutes: timeOfDay.minute))
         .difference(now);
+    var isNegative = difference.isNegative;
 
-    if (difference.inDays >= 7) {
-      var weeks = difference.inDays ~/ 7;
-      return '$weeks週間後';
-    } else if (difference.inDays > 0) {
-      return '${difference.inDays}日後';
-    } else if (difference.inHours > 0) {
-      return '${difference.inHours}時間後';
-    } else if (difference.inMinutes > 0) {
-      return '${difference.inMinutes}分後';
+    if (difference.inDays.abs() >= 365) {
+      var years = difference.inDays.abs() ~/ 365;
+      return isNegative ? '$years年前' : '$years年後';
+    } else if (difference.inDays.abs() >= 30) {
+      var months = difference.inDays.abs() ~/ 30;
+      return isNegative ? '$monthsヶ月前' : '$monthsヶ月後';
+    } else if (difference.inDays.abs() >= 7) {
+      var weeks = difference.inDays.abs() ~/ 7;
+      return isNegative ? '$weeks週間前' : '$weeks週間後';
+    } else if (difference.inDays.abs() > 0) {
+      return isNegative
+          ? '${difference.inDays.abs()}日前'
+          : '${difference.inDays}日後';
+    } else if (difference.inHours.abs() > 0) {
+      return isNegative
+          ? '${difference.inHours.abs()}時間前'
+          : '${difference.inHours}時間後';
+    } else if (difference.inMinutes.abs() > 0) {
+      return isNegative
+          ? '${difference.inMinutes.abs()}分前'
+          : '${difference.inMinutes}分後';
     } else {
-      return '今すぐ';
+      return '現在';
     }
   }
 }
