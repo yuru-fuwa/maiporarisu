c925954df2f73dacd9e9186ce8bd409aec0f21c5
komugi8 <8448eito@gmail.com>
2023-12-02 09:59:11 +0900

    mockの使用

--- lib/data/service/service_constants.dart
+++ lib/data/service/service_constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
   static String taskUrl = '';
+  static String testUser = 'test';
 
   void init() {
     taskUrl = dotenv.env['BASE_URL'] ?? '';
